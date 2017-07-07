c72d9f3c53beca4d5fdbfdad0ed1647278dd622a
Jessie Chatham Spencer <isai@protonmail.com>
2017-07-07 17:35:58 +0200

    Gross domestic product is now displayed in a bar graph.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,46 @@ const d3 = require("d3");
 const dataUrl = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 var data;
 
-
-
 function drawChart(data) {
-  d3.select('.chart')
-      .selectAll('div')
+
+  const svg = d3.select('svg'),
+                margin = {top: 20, right: 20, bottom: 30, left: 40},
+                width = + svg.attr('width') - margin.left - margin.right,
+                height = +svg.attr('height') - margin.top - margin.bottom;
+
+  let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+      y = d3.scaleLinear().rangeRound([height, 0]);
+
+  let g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top +')');
+
+  x.domain(data.map((d) => (d[0])));
+  y.domain([0, d3.max(data, (d) => (d[1]))]);
+
+  g.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(9,' + height +')')
+        .call(d3.axisBottom(x));
+
+  g.append('g')
+        .attr('class', 'axis axis--y')
+        .call(d3.axisLeft(y).ticks(10, '%'))
+      .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('text-anchor', 'end')
+        .text('Frequency')
+
+    g.selectAll('.bar')
       .data(data)
-        .enter()
-        .append('div')
-        .style('width', d => d[1] + 'px')
-        .text((d) => d[1]);
+      .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', (d) => x(d[0]))
+        .attr('y', (d) => y(d[1]))
+        .attr('width', x.bandwidth())
+        .attr('height', (d) => height - y(d[1]));
+  
 }
 
 class App extends Component {
